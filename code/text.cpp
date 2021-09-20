b5add928fb9ddc601dce3d5fc1ff68f1abcb6a4c
Noah Que <904762746@qq.com>
2021-09-20 15:13:45 +0800

    refactor(all): change file structure

---
+++ code/text.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+void test(){
+    printf("asdfasdf  xxd dsff");
+    printf('d');
+}
+int main(){
+    int i=1;
+    double j=0;
+    long f;//sssasddd dcad
+    //asdad
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }/*ddsfsfsfd*/
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            break;
+    }
+    if(i<0){
+        if(i<-1){}
+        else{
+            if(i==2);
+        }
+    }
+    else if(i>0){
+        if (i>2){
+            if(i!=1);
+            else if(i>1);
+        }
+        else if (i==2) {
+            if(i<-1){}
+            else if(i==1){}
+            else{}
+        }
+        else if (i>1) {}
+        else {
+        }
+    }
+    else{
+        if(j!=0){}
+        else{}
+    }
+    switch(i){}
+    return 0;
+}
+/*ds
+asdasdasd
+asdsadasd
+as?
